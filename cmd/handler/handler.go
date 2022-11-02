796deae24ce3e4c678b31e077a6529f29fb1e73f
Arjun Malhotra <am2cj@virginia.edu>
2022-11-02 16:03:28 -0500

    Add functionality for 1. Post a new appointment and 2. Get scheduled appointments of a trainer

--- cmd/handler/handler.go
+++ cmd/handler/handler.go
@@ -19,13 +19,17 @@ func New() *handler {
 	var h handler
 	h.scheduler = scheduler.New()
 	r := chi.NewMux()
-	r.Post("/appointment/", h.postAppointment)
+	r.Post("/appointment", h.postAppointment)
+	r.Get("/get-scheduled-appointments", h.getAppointmentsByTrainer)
+	//r.Get("/get-available", h.getAppointmentsByAvailability)
 	h.Router = r
 	return &h
 }
 
 func (h *handler) postAppointment(res http.ResponseWriter, req *http.Request) {
 	// TODO: Validate the post body.
+	// Validate the required fields. Check endTime - startTime = 30mins
+	// Validate that both start and end times are always only 00 and 30 mins
 	var appt scheduler.Appointment
 	err := json.NewDecoder(req.Body).Decode(&appt)
 	if err != nil {
@@ -34,7 +38,33 @@ func (h *handler) postAppointment(res http.ResponseWriter, req *http.Request) {
 		render.JSON(res, req, "Internal server Error")
 		return
 	}
-	h.scheduler.AddAppointment(appt)
+	err = h.scheduler.AddAppointment(appt)
+	if err != nil {
+		res.WriteHeader(http.StatusBadRequest)
+		render.JSON(res, req, err.Error())
+		return
+	}
 	res.WriteHeader(http.StatusCreated)
 	render.JSON(res, req, appt)
 }
+
+func (h *handler) getAppointmentsByTrainer(res http.ResponseWriter, req *http.Request) {
+	stringTrainerID := req.URL.Query().Get("trainer_id")
+	if stringTrainerID == "" {
+		render.Status(req, http.StatusBadRequest)
+		render.JSON(res, req, "trainerId missing in the query.")
+		return
+	}
+	allSchedules, err := h.scheduler.GetAppointmentsByTrainer(stringTrainerID)
+	if err != nil {
+		log.Printf("getAppointmentsByTrainerByDates: error  %v", err)
+		render.Status(req, http.StatusInternalServerError)
+		render.JSON(res, req, "Internal server Error")
+		return
+	}
+	res.WriteHeader(http.StatusOK)
+	render.JSON(res, req, allSchedules)
+	//fmt.Println("stringTrainerID: ", stringTrainerID)
+}
+
+//func getAppointmentsByAvailability(res http.ResponseWriter, req *http.Request)
--- cmd/scheduler/scheduler.go
+++ cmd/scheduler/scheduler.go
@@ -1,30 +1,128 @@
 package scheduler
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"sort"
+	"strconv"
+	"time"
+)
 
 type Appointment struct {
-	TrainerId int `json:"TrainerId"`
-	UserId    int `json:"UserId"`
-	// appointmentStartDate time.Time
-	// appointmentEndDate   time.Time
+	id                         int
+	TrainerId                  int64  `json:"trainer_id"`
+	UserId                     int64  `json:"user_id"`
+	StringAppointmentStartDate string `json:"starts_at"`
+	StringAppointmentEndDate   string `json:"ends_at"`
+	unixAppointmentStartDate   int64
+	unixAppointmentEndDate     int64
 }
 
-type Scheduler map[int]Appointment
+const timeLayout = "2006-01-02T15:04:05-08:00"
+
+type Scheduler map[int64][]Appointment
 
 func New() *Scheduler {
 	s := make(Scheduler)
 	return &s
 }
 
-func (s Scheduler) AddAppointment(appt Appointment) {
+func (s Scheduler) AddAppointment(appt Appointment) error {
+	apptStartTime, err := time.Parse(timeLayout, appt.StringAppointmentStartDate)
+	if err != nil {
+		log.Println("AddAppointment: error while parsing startTime")
+		return fmt.Errorf("internal server error")
+	}
+	if int(apptStartTime.Weekday()) > 5 {
+		return fmt.Errorf("day should have only be between Monday and Friday")
+	}
+	if apptStartTime.Hour() < 8 || apptStartTime.Hour() > 17 {
+		return fmt.Errorf("first appointment starts at 0800 and last appointment starts at 1630")
+	}
+	fmt.Println(apptStartTime.Minute())
+	if apptStartTime.Minute() != 30 && apptStartTime.Minute() != 0 {
+		return fmt.Errorf("start time should have only 0 or 30 minutes")
+	}
+
+	apptEndTime, err := time.Parse(timeLayout, appt.StringAppointmentEndDate)
+	if err != nil {
+		log.Println("AddAppointment: error while parsing endTime")
+		return fmt.Errorf("internal server error")
+	}
+	if int(apptEndTime.Weekday()) > 5 {
+		return fmt.Errorf("day should have only be between Monday and Friday")
+	}
+	if apptEndTime.Hour() < 8 || apptEndTime.Hour() > 17 {
+		return fmt.Errorf("first appointment ends at 0830 and last appointment ends at 1700")
+	}
+	if apptEndTime.Minute() != 30 && apptEndTime.Minute() != 0 {
+		return fmt.Errorf("end time should have only 00 or 30 minutes")
+	}
+	timeDiff := apptEndTime.Sub(apptStartTime)
+	if timeDiff.Minutes() != 30 {
+		return fmt.Errorf("an appointment should only be 30 minutes of duration")
+	}
+	appt.id = len(s[appt.TrainerId]) + 1
+	appt.unixAppointmentStartDate = apptStartTime.Unix()
+	appt.unixAppointmentEndDate = apptEndTime.Unix()
+
 	if _, ok := s[appt.TrainerId]; !ok {
-		s[appt.TrainerId] = appt
+		var tempList = []Appointment{appt}
+		s[appt.TrainerId] = tempList
+	} else {
+		isOverLap := isStartTimeOverlap(s[appt.TrainerId], appt.unixAppointmentStartDate)
+		if isOverLap {
+			log.Printf("AddAppointment: overlap %v", appt)
+			return fmt.Errorf("there was an overlap, this time slot has already been taken")
+		}
+		s[appt.TrainerId] = append(s[appt.TrainerId], appt)
+		sort.Sort(byStartTime(s[appt.TrainerId]))
 	}
 	s.PrintAppointments()
+	return nil
+}
+
+type byStartTime []Appointment
+
+func (bST byStartTime) Len() int {
+	return len(bST)
+}
+
+func (bST byStartTime) Swap(i, j int) {
+	bST[i], bST[j] = bST[j], bST[i]
+}
+
+func (bST byStartTime) Less(i, j int) bool {
+	return bST[i].unixAppointmentStartDate < bST[j].unixAppointmentStartDate
+}
+
+func isStartTimeOverlap(appointments []Appointment, startTime int64) bool {
+	fmt.Println(appointments, startTime)
+	l := 0
+	r := len(appointments) - 1
+	for l <= r {
+		mid := l + (r-l)/2
+		if appointments[mid].unixAppointmentStartDate == startTime {
+			return true
+		} else if appointments[mid].unixAppointmentStartDate < startTime {
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+	return false
 }
 
 func (s Scheduler) PrintAppointments() {
 	for _, v := range s {
 		fmt.Println(v)
 	}
+}
+
+func (s Scheduler) GetAppointmentsByTrainer(trainerId string) ([]Appointment, error) {
+	intTrainerID, err := strconv.ParseInt(trainerId, 10, 64)
+	if err != nil {
+		return []Appointment{}, fmt.Errorf("GetAppointmentsByTrainer: error converting string to int %v", err)
+	}
+	return s[intTrainerID], nil
 }
